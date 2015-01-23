857ba0c915bde6651f942c6e23aaec5b6c3119fa
Gautestad <oyvind.gautestad@gmail.com>
2015-01-23 12:25:32 +0100

    fjernet rot

--- src/Chessgame.java
+++ src/Chessgame.java
@@ -7,9 +7,5 @@ public class Chessgame {
 
 	public static void main(String[] args) {
 		new ApplicationFrame();
-		JButton btn = new JButton();
-		System.out.println("feef");
-		System.out.println("hei Andrea ");
-		
 	}
 }
