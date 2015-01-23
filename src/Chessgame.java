26fd99222a779cfe967004db635500bbcc32d64a
Gautestad <oyvind.gautestad@gmail.com>
2015-01-23 14:00:23 +0100

    endret litt

--- src/Chessgame.java
+++ src/Chessgame.java
@@ -6,12 +6,6 @@ import gui.ApplicationFrame;
 public class Chessgame {
 
 	public static void main(String[] args) {
-		new ApplicationFrame();
-		new ApplicationFrame();
-		new ApplicationFrame();
-		new ApplicationFrame();
-
-
-		
+		new ApplicationFrame();	
 	}
 }
