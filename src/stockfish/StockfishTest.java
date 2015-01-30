16fe563eeb3e0a8f1be7eec503aa34f6d896bcfb
Marit <Marit@172.19.97.241>
2015-01-30 12:55:38 +0100

    Endringer

--- src/stockfish/StockfishTest.java
+++ src/stockfish/StockfishTest.java
@@ -1,13 +1,20 @@
 package stockfish;
 
+import gui.ApplicationFrame;
+
 public class StockfishTest {
 
 	/**
 	 * @param args
 	 */
+	public static void main(String[] args) {
+		test();
+	}
+	
 	public static void test() {
 		StockfishClient client = new StockfishClient();
-		String FEN = "8/6pk/8/1R5p/3K3P/8/6r1/8 b - - 0 42";
+		//"2r1nr1k/pp1q1p1p/3bpp2/5P2/1P1Q4/P3P3/1B3P1P/R3K1R1"
+		String FEN = "8/6pk/8/1R5p/3K3P/8/6r1/8 b - - 0 42"; 
 		
 		// initialize and connect to engine
 		if (client.startEngine()) {
