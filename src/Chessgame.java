f0058fe0e39cd1b265be7e4ff0424e70574820a7
AndersLarsen <anders.borg.larsen@stundet.hbv.no>
2015-03-07 17:15:44 +0100

    Java doc Chessgame.java

--- src/Chessgame.java
+++ src/Chessgame.java
@@ -1,10 +1,22 @@
 
 import gui.Chessie;
 
-
+/**
+ *This is a chess application that is ment for  the Course ASD-3000
+ *
+ * The Groupe:
+ * Andrea Bøe Abrahamsen
+ * Linda Fermanm
+ * Øyvind Gautestad
+ * Anders Borg Larsen
+ *
+ */
 public class Chessgame {
 
-	
+    /**
+     * This is the main method that starts the application
+     * @param args
+     */
 	public static void main(String[] args) {
 		new Chessie();	
 	}
