0d94f79d4a3b3a26f2edb70be7c47e8bd0b2e3c8
Gautestad <oyvind.gautestad@gmail.com>
2015-03-08 13:40:07 +0100

    Javadoc

--- src/logic/PlayerCoordinator.java
+++ src/logic/PlayerCoordinator.java
@@ -2,12 +2,15 @@ package logic;
 
 import gui.ApplicationConstants;
 
+/**
+ * Klasse som er ansvarlig for å håndtere spiller som er i tur
+ */
 public class PlayerCoordinator {
 	
 	private String playerInTurn = ApplicationConstants.WHITE;
 
     /**
-     * @return
+     * @return hvilken spiller som er i tur
      */
 	public String whoIsInTurn(){
 		return playerInTurn;
@@ -15,14 +18,14 @@ public class PlayerCoordinator {
 
     /**
      *
-     * @return
+     * @return hvilken spiller som vant spillet
      */
 	public String whoWon(){
 		return (playerInTurn.equals(ApplicationConstants.WHITE)) ? ApplicationConstants.BLACK : ApplicationConstants.WHITE;
 	}
 
     /**
-     *
+     * Metode som endrer hvilken spiller som er i tur
      */
 	public void changePlayerInTurn(){
 		playerInTurn = (playerInTurn.equals(ApplicationConstants.WHITE)) ? ApplicationConstants.BLACK : ApplicationConstants.WHITE;
