fb7bf65ac0365d9ad3197b4e249af289483aa367
Gautestad <oyvind.gautestad@gmail.com>
2015-03-08 12:05:21 +0100

    Javadoc Pawn

--- src/gui/pieceGui/Pawn.java
+++ src/gui/pieceGui/Pawn.java
@@ -4,11 +4,20 @@ import gui.ApplicationConstants;
 import logic.pieceLogic.PieceLogicFactory;
 import logic.pieceLogic.PieceMessage;
 
+/**
+ * Klasse Pawn har ansvar for sette riktig bilde, svart eller hvit Pawn, i Piece.
+ * Utvider klassen Piece som er en AbstractButton.
+ */
 public class Pawn extends Piece {
 	
 	
 	private PieceMessage pawnLogic;
-	
+
+    /**
+     * Pawn<p>
+     * Henter bildefil til en svart eller hvit brikke og forepør en instans av pawnLogic fra PiecLogicFactory
+     * @param isBlack hvorvidt en brikke er svart eller ikke
+     */
 	public Pawn(boolean isBlack) {
 		if(isBlack){
 			setImage("img/Black_Pawn.png");
@@ -20,11 +29,20 @@ public class Pawn extends Piece {
 		}
 	}
 
+    /**
+     * Metode som returnerer FENSymbol fra pawnLogic som brukes for å identifisere brikken i en FEN string
+     * @return String med FENsymbol
+     */
 	@Override
 	public String getFENSymbol() {
 		return pawnLogic.getFENSymbol();
 	}
 
+    /**
+     * Metode som sender melding til pawnLogic om hvilken kolonne og rad som er trykket på av bruker
+     * @param column kolonne som er trykket på
+     * @param row rad som et trykket på
+     */
 	@Override
 	public void pressed(int column, int row) {
 		pawnLogic.pressed(column, row);
