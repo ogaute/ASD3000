b38a4e0f9d231f42cd9e76ca03183212773c8984
AndersLarsen <anders.borg.larsen@stundet.hbv.no>
2015-03-08 16:49:24 +0100

    Java docing

--- src/gui/Chessie.java
+++ src/gui/Chessie.java
@@ -1,19 +1,22 @@
 package gui;
 
+/**
+ *  Denne klassen har ansvar for å starte eller avslutte et nytt spill
+ */
 public class Chessie {
-	
-	
+
+
 	private static ApplicationFrame application;
 
     /**
-     *
+     * konstruktøren som instansierer en ny ApplicationFrame
      */
 	public Chessie() {
 		 application = new ApplicationFrame();
 	}
 
     /**
-     *
+     * metode som har i oppgave og restarte applikasjoen
      */
 	public static void restart(){
 		application.dispose();
