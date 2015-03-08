591a56602257154c4e1914492e5d57594786b67c
Gautestad <oyvind.gautestad@gmail.com>
2015-03-08 11:43:17 +0100

    Javadoc Bishop

--- src/gui/pieceGui/Bishop.java
+++ src/gui/pieceGui/Bishop.java
@@ -4,11 +4,20 @@ import gui.ApplicationConstants;
 import logic.pieceLogic.PieceLogicFactory;
 import logic.pieceLogic.PieceMessage;
 
+/**
+ * Klasse Bishop har ansvar for sette riktig bilde, svart eller hvit Bishop, i Piece.
+ * Utvider klassen Piece som er en AbstractButton.
+ */
 public class Bishop extends Piece {
 	
 	
 	private PieceMessage bishopLogic;
-	
+
+    /**
+     * Bishop<p>
+     * Henter bildefil til en svart eller hvit brikke og forepør tilsvarende instans av bishopLogic fra PiecLogicFactory
+     * @param isBlack hvorvidt en brikke er svart eller ikke
+     */
 	public Bishop(boolean isBlack) {
 		if(isBlack){
 			setImage("img/Black_Bishop.png");
@@ -20,11 +29,20 @@ public class Bishop extends Piece {
 		}
 	}
 
+    /**
+     * Metode som returnerer FENSymbol fra bishopLogic som brukes for å identifisere brikken i en FEN string
+     * @return String med FENsymbol
+     */
 	@Override
 	public String getFENSymbol() {
 		return bishopLogic.getFENSymbol();
 	}
 
+    /**
+     * Metode som sender hvilken kolonne og rad som er trykket på av bruker til bishopLogic
+     * @param column kolonne som er trykket på
+     * @param row rad som et trykket på
+     */
 	@Override
 	public void pressed(int column, int row) {
 		bishopLogic.pressed(column, row);
