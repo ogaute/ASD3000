94b0989ac2a0d389c51734d0fe19158d8833b183
AndreaBoeAbrahamsen <Andrea.B.Abrahamsen@student.hbv.no>
2015-03-08 13:23:24 +0100

    tårn logikk javadoc

--- src/logic/pieceLogic/RookLogic.java
+++ src/logic/pieceLogic/RookLogic.java
@@ -3,14 +3,18 @@ package logic.pieceLogic;
 import controller.Controller;
 import gui.ApplicationConstants;
 
+/**
+ * Logic klasse for tårn brikker. <p>
+ * Tar ansvar for å holde på farge, brikke symbol og se etter lovlige bevegelser ved trykk.
+ */
 class RookLogic extends PieceLogic implements PieceMessage{
 
 	
 	private String pieceColor;
 
     /**
-     *
-     * @param pieceColor
+     * Konstruktør, tar i mot hvilken farge brikken skal være.
+     * @param pieceColor brikkens farge
      */
 	protected RookLogic(String pieceColor) {
 		this.pieceColor = pieceColor;
@@ -18,7 +22,7 @@ class RookLogic extends PieceLogic implements PieceMessage{
 	}
 
     /**
-     *
+     * Setter egenskaper (symbol) til brikkelogikken basert på fargen til brikken
      */
 	private void setPieceProperties(){
 		if(pieceColor.equals(ApplicationConstants.BLACK)){
@@ -30,9 +34,10 @@ class RookLogic extends PieceLogic implements PieceMessage{
 	}
 
     /**
-     *
-     * @param column
-     * @param row
+     * Metode som tar i mot om brikken er presset på gitt kolonne og rad for forflyttning og ser etter
+     * lovlige trekk for tårn brikken. 
+     * @param column kolonne som brikken er på
+     * @param row rad som brikken er på
      */
 	public void pressed(int column, int row) {
 		if(pieceColor.equals(Controller.getPlayerInTurn())){
