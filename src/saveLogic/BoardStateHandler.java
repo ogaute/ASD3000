918a27d0d783c0850828cf7e0d833d4dcca53efd
liferm <linda.fermann@gmail.com>
2015-03-08 12:52:33 +0100

    javadoc

--- src/saveLogic/BoardStateHandler.java
+++ src/saveLogic/BoardStateHandler.java
@@ -2,39 +2,45 @@ package saveLogic;
 
 import gui.pieceGui.Piece;
 
-/*
- Lager og lagrer tilstanden i et BoardState objekt.
- (Originator)
+/**
+ * Klassen BoardStateHandler har ansvar for å håndtere nåværende stilling
+ * på brettet og å lage en ny stilling på brettet.
+ * 
+ * Klassen er med på å skape mønsteret Memento, og fungerer som originator.
  */
+
 public class BoardStateHandler {
+	
+	
 	private Piece[][] boardState;
 
     /**
-     *
-     * @param boardState
+     * Setter klassefeltvariabel til å være lik 
+     * det som mottas som innparameter.
+     * 
+     * @param boardState Et todemensjonalt array av objekter av typen Piece
      */
 	public void setBoardState(Piece[][] boardState) {
 		this.boardState = boardState;
 	}
 
     /**
-     *
-     * @return
+     * Returnerer brettstillingen
+     * 
+     * @return Returnerer innholdet i klassefeltvariabelen boardState
      */
 	public Piece[][] getBoardState() {
 		return boardState;
 	}
 
     /**
-     *
-     * @return
+     * Lagrer en ny brettstilling
+     * 
+     * @return Returnerer et nytt objekt av typen BoardState som mottar brettstillingen som innparameter
      */
 	public BoardState saveBoardState() {
 		return new BoardState(boardState);
 	}
 
-//	ikke i bruk
-// public void getHistoryBoardState(BoardState BoardState) {
-//		boardState = BoardState.getBoardState();
-//	}
+
 }
