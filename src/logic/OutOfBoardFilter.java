b4185909b45757e61bc3ea3919903a198a36d680
Gautestad <oyvind.gautestad@gmail.com>
2015-03-08 13:38:04 +0100

    Javadoc

--- src/logic/OutOfBoardFilter.java
+++ src/logic/OutOfBoardFilter.java
@@ -2,13 +2,16 @@ package logic;
 
 import gui.ApplicationConstants;
 
+/**
+ *  Klasse som er ansvarlig for å filtrere bort felt (kolonne, rad) som ligger på utsiden av sjakkbrettet
+ */
 public class OutOfBoardFilter {
 
     /**
-     *
-     * @param column
-     * @param row
-     * @return
+     * Metode som validerer om en gitt colonne og rad ligger på utsiden av sjakkbrettet
+     * @param column kolonne som skal sjekkes
+     * @param row rad som skal sjekkes
+     * @return om felt er utenfor eller ikke
      */
 	public static boolean isOutOfBoard(int column, int row){
 		boolean isOut = true;
