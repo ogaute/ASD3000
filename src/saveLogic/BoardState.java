ec769c53966beac94483f4a299311404fbd79488
liferm <linda.fermann@gmail.com>
2015-03-08 12:36:02 +0100

    javadoc

--- src/saveLogic/BoardState.java
+++ src/saveLogic/BoardState.java
@@ -2,26 +2,36 @@ package saveLogic;
 
 import gui.pieceGui.Piece;
 
-/*
-Den holder på tilstanden til et objekt  som kan gjennopprettes.
-(Memento)
+/**
+ * Klassen BoardState har som ansvar å holde på tilstanden til en 
+ * spillebrettstilling som kan gjenopprettes ved en senere anledning.
+ * 
+ * Klassen er med på å skape mønsteret Memento
  */
+
 public class BoardState {
+	
+	
 	private Piece[][] boardState;
 
     /**
+     * Konstruktør. Setter klassefeltvariabel til å være lik 
+     * det som mottas som innparameter.
      *
-     * @param boardState
+     * @param boardState Et todemensjonalt array av objekter av typen Piece
      */
 	public BoardState(Piece[][] boardState) {
 		this.boardState = boardState;
 	}
 
     /**
-     *
-     * @return
+     * Returnerer brettstillingen.
+     * 
+     * @return Returnerer innholdet i klassefeltvariabelen boardState
      */
 	public Piece[][] getBoardState() {
 		return boardState;
 	}
+	
+	
 }
